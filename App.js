9ae7852e2647514e17abffffc6942f34cf8d7d1d
EnsafCSharpProj <saadettahiri99@gmail.com>
2021-12-08 17:41:34 +0100

    add pagination scrool

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [currentPage, setcurrentPage] = useState(1);
+  const getallUsers = async () => {
+    await axios
+      .get(`https://randomuser.me/api/?page=${currentPage}&results=10`)
+      .then((res) => {
+        setUsers([...users,...res.data.results]);
+      });
+  };
+  useEffect(() => {
+    getallUsers();
+  }, [currentPage]);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          marginTop: "5%",
+          flexDirection: "row",
+          paddingHorizontal: 16,
+          paddingVertical: 16,
+          borderBottomWidth: 1,
+          borderColor: "black",
+        }}
+      >
+        <Image
+          source={{ uri: item.picture.large }}
+          style={{ marginRight: 16, width: 100, height: 100 }}
+        />
+        <View>
+          <Text>{`${item.name.title}`}</Text>
+          <Text>{`${item.email}`}</Text>
+        </View>
+      </View>
+    );
+  };
+  const loadMoreItem=()=>{
+    setcurrentPage(currentPage +1);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={users}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.email}
+      ListFooterComponent={renderLoader}
+      onEndReached={loadMoreItem}
+      onEndReachedThreshold={0}
+    />
   );
 }
-
+const renderLoader =()=>{
+  return(
+    <View style={{marginVertical:16 }}>
+      <ActivityIndicator size="large" color="#aaa" />
+    </View>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
